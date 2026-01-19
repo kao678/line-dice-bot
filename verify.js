64fd4374690d02946bf0932d5f4a62a5b583aad9
kao678 <kaobas2536@gmail.com>
2026-01-20 01:05:30 +0700

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,19 +1,23 @@
 const crypto = require("crypto");
 
 module.exports = (body, signature, secret) => {
-  if (!signature) return false;
-
-  const hash = crypto
-    .createHmac("sha256", secret)
-    .update(body)
-    .digest("base64");
-
   try {
+    if (!signature) return false;
+
+    const hash = crypto
+      .createHmac("sha256", secret)
+      .update(body)
+      .digest("base64");
+
+    // ป้องกัน length ไม่เท่ากัน (กัน crash)
+    if (hash.length !== signature.length) return false;
+
     return crypto.timingSafeEqual(
       Buffer.from(hash),
       Buffer.from(signature)
     );
-  } catch {
+  } catch (e) {
+    console.error("VERIFY ERROR", e);
     return false;
   }
 };
